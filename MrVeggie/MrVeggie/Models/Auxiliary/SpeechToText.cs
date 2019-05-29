a9616135b456354620c1627c6a5fc722486e509f
Henrique Pereira <pereirapalmeirahenrique@gmail.com>
2019-05-29 13:23:54 +0100

    speech recognizer (initial)

--- MrVeggie/MrVeggie/Models/Auxiliary/SpeechToText.cs
+++ MrVeggie/MrVeggie/Models/Auxiliary/SpeechToText.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.CognitiveServices.Speech;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,38 @@ namespace MrVeggie.Models.Auxiliary
 {
     public class SpeechToText
     {
+
+        public string listen()
+        {
+            var config = SpeechConfig.FromSubscription("652842a020de4fc9990d5cfc8f82fb98", "westeurope");
+
+            var language = "pt-PT";
+            config.SpeechRecognitionLanguage = language;
+
+            var recognizer = new SpeechRecognizer(config);
+
+            string result = "";
+
+            recognizer.StartContinuousRecognitionAsync();
+
+            recognizer.Recognizing += (s, e) => {
+                if (e.Result.Text.Contains("próximo"))
+                {
+                    result = "próximo";
+                    recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+                }
+
+                if (e.Result.Text.Contains("anterior"))
+                {
+                    result = "anterior";
+                    recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+                }
+
+                Console.WriteLine(e.Result.)
+
+            };
+
+            return result;
+        }
     }
 }
