74bfc8621537517a446e8873d198fb39d8941369
Pedro Moreira <pedrombm19@gmail.com>
2019-06-01 18:51:32 +0100

    Ingredientes agenda; mandar por email

--- MrVeggie/MrVeggie/Controllers/AgendaViewController.cs
+++ MrVeggie/MrVeggie/Controllers/AgendaViewController.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MrVeggie.Contexts;
 using MrVeggie.Models;
+using MrVeggie.Models.Auxiliary;
 using MrVeggie.Models.Pages;
+using System.Net.Mail;
 
 namespace MrVeggie.Controllers {
 
@@ -14,10 +16,11 @@ namespace MrVeggie.Controllers {
 
 
         private Sugestao sugestao { get; set; }
+        private Preparacao preparacao { get; set; }
 
-
-        public AgendaViewController(ReceitaContext context_r, UtilizadorContext context_u, AgendaContext context_a, IngredienteContext context_i) {
+        public AgendaViewController(ReceitaContext context_r, UtilizadorContext context_u, AgendaContext context_a, IngredienteContext context_i, PassoContext context_p, IngredientesPassoContext context_ip) {
             sugestao = new Sugestao(context_r, context_u, null, context_a);
+            preparacao = new Preparacao(context_r, context_p, context_ip, context_i);
         }
 
 
@@ -26,12 +29,63 @@ namespace MrVeggie.Controllers {
             List<Agenda> agenda = sugestao.getAgenda(User.Identity.Name);
             List<Receita> receitas = sugestao.getReceitas();
 
-            AgendaReceitas ai = new AgendaReceitas {
+            AgendaReceitas ar = new AgendaReceitas {
                 agenda = agenda,
                 receitas = receitas
             };
 
-            return View(ai);
+            return View(ar);
         }
+
+        public IActionResult ShowIngredientesAgenda() {
+
+            List<Agenda> agenda = sugestao.getAgenda(User.Identity.Name);
+            List<Receita> receitas = new List<Receita>();
+
+            foreach(Agenda a in agenda) {
+                if(a.receita_id != -1) receitas.Add(preparacao.getIngredientes(a.receita_id));
+            }
+
+            Dictionary<Ingrediente, Quantidade> ingredientes = new Dictionary<Ingrediente, Quantidade>();
+
+            foreach (Receita r in receitas) {
+                foreach (Passo p in r.passos) {
+                    p.ingredientes = preparacao.getIngredientesPasso(p.id_passo);
+                    foreach (var i in p.ingredientes) {
+                        if (!ingredientes.ContainsKey(i.Key)) {
+                            Quantidade q = new Quantidade(0, i.Value.unidade);
+                            ingredientes.Add(i.Key, q);
+                        }
+                        ingredientes[i.Key].add(i.Value.quantidade);
+
+                    }
+                }
+            }
+            
+
+            return View(ingredientes);
+        }
+
+
+
+
+
+        [HttpPost]
+        public void sendEmail([FromBody] string body) {
+            MailMessage mail = new MailMessage();
+            SmtpClient client = new SmtpClient("smtp.gmail.com");
+
+            mail.From = new MailAddress("mr.veggiept@gmail.com");
+            mail.To.Add(User.Identity.Name);
+            mail.Subject = "Lista de ingredientes semanal";
+            mail.Body = body;
+
+            client.Port = 587;
+            client.Credentials = new System.Net.NetworkCredential("mr.veggiept@gmail.com", "li4grupo25");
+            client.EnableSsl = true;
+            
+            client.Send(mail);
+        }
+
     }
 }
